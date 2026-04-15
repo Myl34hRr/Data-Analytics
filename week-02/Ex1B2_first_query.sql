847f657ecf580255223d989d70bf42c1d8f5fc21
Myleah.Richardson <richardsonmyleah89@gmail.com>
2026-04-15 06:34:51 -0700

    Ex1B2_first_query.sql

---
+++ week-02/Ex1B2_first_query.sql
@@ -0,0 +1,9 @@
+/*
+The pupose of Sakila database is to help us learn SQL and how to organize the database 
+*/
+
+USE sakila;
+SELECT * FROM actor;
+select * FROM actore; -- Retrieved (200) records
+select * FROM film;
+select * FROM film; -- Retrieved (1000) records 
