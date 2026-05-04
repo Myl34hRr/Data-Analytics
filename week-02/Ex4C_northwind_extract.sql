99961f6b1537c3951ec73fd1afc6e78ffe6c5099
Myleah.Richardson <richardsonmyleah89@gmail.com>
2026-05-04 08:03:08 -0700

    git commit -m

---
+++ week-02/Ex4C_northwind_extract.sql
@@ -0,0 +1,13 @@
+/*
+a) the products table holds the northwind items 
+b) categories table holds the type/categories of the items in northwind sells 
+SELECT * FROM employees;
+a) the employee with the name like a bird is peacock margaret 
+SELECT * FROM products 
+a) the quer returns 77 records, by going to the tool bar at the top select query, there is a limit rows option click that and choose limit to 10 rows 
+SELECT * FROM categories 
+c) the categoryid # for seafood is 8
+SELECT orderid, orderdate, shipname, shipcountry 
+FROM orders 
+LIMIT 50; 
+
--- week-03/Northwind_practice_questions.sql
+++ week-03/Northwind_practice_questions.sql
@@ -1,19 +1,58 @@
 USE northwind;
+
 SELECT categoryid, categoryName , description 
 FROM categories;
+
 SELECT productid, productName
 FROM products
 WHERE discontinued = 1;
+
 SELECT FirstName, ' ', lastname AS 'fullname'
 FROM employees; 
+
 SELECT CustomerID, CompanyName, Country
 FROM customers
 WHERE country IN ('Germany' , 'France');
+
 SELECT COUNT(orderid)AS 'TotalOrders'
 FROM orders;
+
+
 SELECT supplierid, CompanyName, ContactName,ContactTitle
 FROM suppliers 
-WHERE contactTitles IN ('Sales representative' , 'Sales Manger')
+WHERE contactTitle IN ('Sales representative' , 'Sales Manager');
+-- where contactTitles == 'Sales Representative" or  "Sales manager"
+
+SELECT customerID, contactName, orderid, companyName, OrderDate, shipAddress,address
+FROM orders
+JOIN customers USING (CustomerID)
+ORDER BY orderdate;
+-- LIMIT 5;
+
+SELECT o.customerID, contactName, orderid, companyName, OrderDate, shipAddress,address,shipCity,city
+FROM orders o
+JOIN customers c ON o.shipCity = c.city
+ORDER BY orderdate;
+-- LIMIT 5;
+
+-- Example 3
+SELECT p. ProductName,
+c. CategoryName,
+p.UnitPrice
+FROM products p
+INNER JOIN categories c ON p.CategoryID = c.CategoryID
+ORDER BY c. CategoryName, p.ProductName
+LIMIT 6;
+-- Example 5
+-- custmores with zero oders will show 0 in order count 
+
+SELECT c. companyName, 
+COUNT(o.orderid) AS 'order count'
+FROM customers c 
+LEFT JOIN orders o ON c.CustomerID = o.CustomerID
+GROUP BY C.companyName
+ORDER BY 'order count' ASC
+LIMIT 5;
 
 
 
